5083afe5becf87aa98a2b26108e7c8e86da249a1
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-04-18 21:33:41 -0500

    Fix syntax error

--- frontend_server/src/queryobjseg_segmentations.erl
+++ frontend_server/src/queryobjseg_segmentations.erl
@@ -144,7 +144,7 @@ from_json(Json) ->
     A
   catch
     _: {badkey, Key} -> {error, <<"missing field: ", Key/binary>>};
-    _: {bad_type, Key} -> {error, <<"missing field: ", Key/binary>>};
+    _: {bad_type, Key} -> {error, <<"missing field: ", Key/binary>>}
   end.
 
 -spec update(Segmentation::segmentation(), Json::sumo_rest_doc:json()) ->
