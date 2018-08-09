ef47ca01db649b722b003a68a1935b1f7e3a3b7b
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-08-08 22:31:28 -0500

    Hardcode VPN endpoint

--- misc/upload.py
+++ misc/upload.py
@@ -47,115 +47,114 @@ input_transform = Compose([
         std=[0.229, 0.224, 0.225])
 ])
 
-for dataset in ReferDataset.SUPPORTED_DATASETS:
-    if dataset == 'referit':
-        continue
-    print("Processing {0}".format(dataset))
-    args.dataset = dataset
-    if 'params' in ReferDataset.SUPPORTED_DATASETS[dataset]:
-        refer_db = REFER(osp.join(args.data, 'other'),
-                         **ReferDataset.SUPPORTED_DATASETS[dataset]['params'])
-    for split in ReferDataset.SUPPORTED_DATASETS[dataset]['splits']:
-        args.split = split
-        print('Processing {0}'.format(split))
-        refer = ReferDataset(data_root=args.data,
-                             split_root='../../query-objseg/data',
-                             dataset=args.dataset,
-                             split=args.split,
-                             transform=input_transform,
-                             max_query_len=args.time)
-        first_entry = None
-        prev_entry = None
+# for dataset in ReferDataset.SUPPORTED_DATASETS:
+    # if dataset == 'referit':
+        # continue
+print("Processing {0}".format(args.dataset))
+dataset = args.dataset
+if 'params' in ReferDataset.SUPPORTED_DATASETS[dataset]:
+    refer_db = REFER(osp.join(args.data, 'other'),
+                     **ReferDataset.SUPPORTED_DATASETS[dataset]['params'])
+for split in ReferDataset.SUPPORTED_DATASETS[dataset]['splits']:
+    args.split = split
+    print('Processing {0}'.format(split))
+    refer = ReferDataset(data_root=args.data,
+                         split_root='../../query-objseg/data',
+                         dataset=args.dataset,
+                         split=args.split,
+                         transform=input_transform,
+                         max_query_len=args.time)
+    first_entry = None
+    prev_entry = None
 
-        net = DMN(dict_size=len(refer.corpus),
-                 emb_size=args.emb_size,
-                 hid_size=args.hid_size,
-                 vis_size=args.vis_size,
-                 num_filters=args.num_filters,
-                 mixed_size=args.mixed_size,
-                 hid_mixed_size=args.hid_mixed_size,
-                 lang_layers=args.lang_layers,
-                 mixed_layers=args.mixed_layers,
-                 backend=args.backend,
-                 mix_we=args.mix_we,
-                 lstm=args.lstm,
-                 high_res=args.high_res,
-                 upsampling_mode=args.upsamp_mode,
-                 upsampling_size=args.upsamp_size,
-                 upsampling_amplification=args.upsamp_amplification)
+    net = DMN(dict_size=len(refer.corpus),
+             emb_size=args.emb_size,
+             hid_size=args.hid_size,
+             vis_size=args.vis_size,
+             num_filters=args.num_filters,
+             mixed_size=args.mixed_size,
+             hid_mixed_size=args.hid_mixed_size,
+             lang_layers=args.lang_layers,
+             mixed_layers=args.mixed_layers,
+             backend=args.backend,
+             mix_we=args.mix_we,
+             lstm=args.lstm,
+             high_res=args.high_res,
+             upsampling_mode=args.upsamp_mode,
+             upsampling_size=args.upsamp_size,
+             upsampling_amplification=args.upsamp_amplification)
 
-        net = net.cuda()
-        net.load_state_dict(torch.load('../../query-objseg-weights/highres/'
-                            'dmn_{0}_weights.pth'.format(args.dataset)))
+    net = net.cuda()
+    net.load_state_dict(torch.load('../../query-objseg-weights/highres/'
+                        'dmn_{0}_weights.pth'.format(args.dataset)))
 
-        for idx in tqdm.tqdm(range(0, 5)):
-            img_file, mask_file, text_phrase = refer.images[idx]
-            re_match = regex.match(img_file)
-            img_id = int(re_match.group(1))
-            img, mask, phrase = refer[idx]
-            img = img.unsqueeze(0).cuda()
-            phrase = phrase.unsqueeze(0).cuda()
-            with torch.no_grad():
-                out_mask = net(img, phrase)
-            out_mask = F.upsample(out_mask, size=(mask.size(-1), mask.size(1)),
-                                  mode='bilinear')
-            out_mask = F.sigmoid(out_mask)
-            out_mask = out_mask.cpu().numpy()
-            mask = mask.unsqueeze(-1).numpy().astype(np.uint8)
-            coco_mask = cocomask.encode(mask)[0]
-            def find_ids():
-                anns = [r for r in refer_db.anns
-                        if refer_db.anns[r]['image_id'] == img_id]
-                for ann_id in anns:
-                    possible_refs = [refer_db.refs[r] for r in refer_db.refs
-                                     if refer_db.refs[r]['ann_id'] == ann_id]
-                    for ref in possible_refs:
-                        for sentence in ref['sentences']:
-                            if sentence['sent'] == text_phrase:
-                                return (ann_id, ref['ref_id'],
-                                        sentence['sent_id'])
-            ann_id = ref_id = sent_id = None
-            if args.dataset != 'referit':
-                ann_id, ref_id, sent_id = find_ids()
-            img_url = refer_db.imgs[img_id]['flickr_url']
-            req_body = {'id': str(sent_id), 'ref_id': str(ref_id),
-                        'ann_id': str(ann_id), 'img_id': str(img_id),
-                        'img_url': img_url, 'dataset': args.dataset,
-                        'split': args.split, 'query_expr': text_phrase,
-                        'mask': coco_mask['counts']}
-            print(req_body)
-            if first_entry is None:
-                first_entry = req_body
-                prev_entry = req_body
-                try:
-                    requests.post('http://localhost:4892/datasets', json={
-                        'name': args.dataset, 'split': args.split,
-                        'start_id': req_body['id']})
-                except:
-                    pass
-            else:
-                req_body['prev_id'] = prev_entry['id']
-                prev_entry['next_id'] = req_body['id']
-                if idx > 1:
-                    try:
-                        requests.post('http://localhost:4892/datasets/{0}'.format(
-                            args.dataset), json=prev_entry)
-                    except:
-                        pass
-                prev_entry = req_body
-            store_path = '../masks/{0}/{1}'.format(args.dataset, args.split)
+    for idx in tqdm.tqdm(range(0, len(refer.images))):
+        img_file, mask_file, text_phrase = refer.images[idx]
+        re_match = regex.match(img_file)
+        img_id = int(re_match.group(1))
+        img, mask, phrase = refer[idx]
+        img = img.unsqueeze(0).cuda()
+        phrase = phrase.unsqueeze(0).cuda()
+        with torch.no_grad():
+            out_mask = net(img, phrase)
+        out_mask = F.upsample(out_mask, size=(mask.size(-2), mask.size(-1)),
+                              mode='bilinear')
+        out_mask = F.sigmoid(out_mask)
+        out_mask = out_mask.cpu().numpy()
+        mask = mask.unsqueeze(-1).numpy().astype(np.uint8)
+        coco_mask = cocomask.encode(mask)[0]
+        def find_ids():
+            anns = [r for r in refer_db.anns
+                    if refer_db.anns[r]['image_id'] == img_id]
+            for ann_id in anns:
+                possible_refs = [refer_db.refs[r] for r in refer_db.refs
+                                 if refer_db.refs[r]['ann_id'] == ann_id]
+                for ref in possible_refs:
+                    for sentence in ref['sentences']:
+                        if sentence['sent'] == text_phrase:
+                            return (ann_id, ref['ref_id'],
+                                    sentence['sent_id'])
+        ann_id = ref_id = sent_id = None
+        if args.dataset != 'referit':
+            ann_id, ref_id, sent_id = find_ids()
+        img_url = refer_db.imgs[img_id]['flickr_url']
+        req_body = {'id': str(sent_id), 'ref_id': str(ref_id),
+                    'ann_id': str(ann_id), 'img_id': str(img_id),
+                    'img_url': img_url, 'dataset': args.dataset,
+                    'split': args.split, 'query_expr': text_phrase,
+                    'mask': coco_mask['counts']}
+        if first_entry is None:
+            first_entry = req_body
+            prev_entry = req_body
             try:
-                os.makedirs(store_path)
+                requests.post('http://localhost:4892/datasets', json={
+                    'name': args.dataset, 'split': args.split,
+                    'start_id': req_body['id']})
             except:
                 pass
-            with open(osp.join(store_path, req_body['id']), 'wb') as f:
-                f.write(out_mask.tobytes())
+        else:
+            req_body['prev_id'] = prev_entry['id']
+            prev_entry['next_id'] = req_body['id']
+            if idx > 1:
+                try:
+                    requests.post('http://10.241.243.33:4892/datasets/{0}'.format(
+                        args.dataset), json=prev_entry)
+                except:
+                    pass
+            prev_entry = req_body
+        store_path = '../masks/{0}/{1}'.format(args.dataset, args.split)
+        try:
+            os.makedirs(store_path)
+        except:
+            pass
+        with open(osp.join(store_path, req_body['id']), 'wb') as f:
+            f.write(out_mask.tobytes())
 
-        prev_entry['next_id'] = first_entry['id']
-        first_entry['prev_id'] = prev_entry['id']
-        for entry in zip(prev_entry, first_entry):
-            requests.post('http://localhost:4892/datasets/{0}'.format(
-                args.dataset), json=entry)
-        net = net.cpu()
-        del net
-        torch.cuda.empty_cache()
+    prev_entry['next_id'] = first_entry['id']
+    first_entry['prev_id'] = prev_entry['id']
+    for entry in zip(prev_entry, first_entry):
+        requests.post('http://10.241.243.33:4892/datasets/{0}'.format(
+            args.dataset), json=entry)
+    net = net.cpu()
+    del net
+    torch.cuda.empty_cache()
