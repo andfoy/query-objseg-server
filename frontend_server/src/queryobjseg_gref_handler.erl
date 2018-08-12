7ee50a804821c6ba56b61f4ea22fe7ca2099b3e4
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-08-12 18:29:42 -0500

    Add remaining datasets

---
+++ frontend_server/src/queryobjseg_gref_handler.erl
@@ -0,0 +1,86 @@
+%%% @doc POST|GET /datasets/gref handler.
+-module(queryobjseg_gref_handler).
+
+-behaviour(trails_handler).
+
+-ifndef(PRINT).
+-define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
+-endif.
+
+-include_lib("mixer/include/mixer.hrl").
+-mixin([{ sr_entities_handler
+        , [ init/3
+          , rest_init/2
+          , allowed_methods/2
+          , resource_exists/2
+          , content_types_accepted/2
+          , content_types_provided/2
+          , handle_get/2
+          , handle_post/3
+          ]
+        }]).
+
+%% Alias
+-type state() :: sr_entities_handler:state().
+
+-export([trails/0, handle_post/2]).
+
+-spec trails() -> trails:trails().
+trails() ->
+  RequestBody =
+    #{ name => <<"request body">>
+     , in => body
+     , description => <<"request body (as json)">>
+     , required => true
+     },
+  Metadata =
+    #{ get =>
+       #{ tags => ["segmentations"]
+        , description => "Returns the list of segmentations"
+        , produces => ["application/json"]
+        }
+     , post =>
+       # { tags => ["segmentations"]
+         , description => "Creates a new segmentation"
+         , consumes => ["application/json", "application/json; charset=utf-8"]
+         , produces => ["application/json"]
+         , parameters => [RequestBody]
+         }
+     },
+  Path = "/datasets/gref",
+  Options = #{path => Path, model => queryobjseg_gref, verbose => true},
+  [trails:trail(Path, ?MODULE, Options, Metadata)].
+
+-spec handle_post(Req::cowboy_req:req(), State::state()) ->
+  { {true, binary()} | false | halt
+  , cowboy_req:req()
+  , state()
+  }.
+handle_post(Req, State) ->
+  try
+    {ok, Body, Req1}      = cowboy_req:body(Req),
+    Json                  = sr_json:decode(Body),
+    % {DeviceId, _Req} = cowboy_req:binding(device_id, Req),
+    % ?PRINT(DeviceId),
+    % Checks that the given device does exists
+    Dataset = maps:get(<<"dataset">>, Json),
+    case queryobjseg_datasets_repo:exists(Dataset) of
+      true ->
+        case queryobjseg_unc:from_json(Json) of
+          {error, Reason} ->
+            Req2 = cowboy_req:set_resp_body(sr_json:error(Reason), Req1),
+            {false, Req2, State};
+          {ok, Entity} ->
+            handle_post(Entity, Req1, State)
+        end;
+      false ->
+        {ok, Req1} = cowboy_req:reply(404, Req),
+        {halt, Req1, State}
+    end
+  catch
+    _:badjson ->
+      Req3 =
+        cowboy_req:set_resp_body(
+          sr_json:error(<<"Malformed JSON request ">>), Req),
+      {false, Req3, State}
+  end.
---
+++ frontend_server/src/queryobjseg_referit.erl
@@ -0,0 +1,41 @@
+
+-module(queryobjseg_referit).
+
+-include_lib("mixer/include/mixer.hrl").
+-mixin([{ queryobjseg_results
+        , [ sumo_sleep/1
+          , sumo_wakeup/1
+          , to_json/1
+          , from_json/1
+          , update/2
+          , location/2
+          , duplication_conditions/1
+          , new/9
+          , id/1
+          ]}]).
+
+-export([sumo_schema/0]).
+
+-spec sumo_schema() -> sumo:schema().
+sumo_schema() ->
+  % ListSchema = plain_schema(),
+  sumo:new_schema(
+    ?MODULE,
+    [ sumo:new_field(id, binary, [id, unique])
+    , sumo:new_field(ref_id, binary, [not_null])
+    , sumo:new_field(img_id, binary, [not_null])
+    , sumo:new_field(ann_id, binary, [not_null])
+    , sumo:new_field(img_url, binary, [not_null])
+    , sumo:new_field(split, binary, [not_null])
+    , sumo:new_field(query_expr, binary, [not_null])
+    , sumo:new_field(mask, binary, [not_null])
+    , sumo:new_field(prev_id, binary, [not_null])
+    , sumo:new_field(next_id, binary, [not_null])
+    ]).
+
+% -export([from_json/1]).
+%
+% -spec from_json(Json::sumo_rest_doc:json()) ->
+%   {ok, queryobjseg_results:result()} | {error, iodata()}.
+% from_json(Json) ->
+%   queryobjseg_results:from_json(Json).
